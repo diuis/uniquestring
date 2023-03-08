9322e040626987cbb9dcef73195a6907fd732ff8
Demis Gallisto <demis.gallisto@wisemotions.com>
2023-03-08 15:40:28 +0100

    use a switch statement

--- src/main/java/com/demisgallisto/uniquestring/UniqueStringPredicate.java
+++ src/main/java/com/demisgallisto/uniquestring/UniqueStringPredicate.java
@@ -5,18 +5,24 @@ import java.util.function.Predicate;
 import static java.util.Objects.requireNonNull;
 
 public class UniqueStringPredicate implements Predicate<String> {
+
   @Override
   public boolean test(String stringToValidate) {
 
     requireNonNull(stringToValidate, () -> "the string to validate must be not null");
-    if (stringToValidate.isEmpty()) {
-      return true;
-    }
-    if (stringToValidate.length() == 1) {
-      return true;
-    }
-    var distinctChars = stringToValidate.chars().distinct().toArray();
-    var isUnique = distinctChars.length == stringToValidate.length();
-    return isUnique;
+    return switch (stringToValidate.length()) {
+      case 0, 1 -> true;
+      default -> stringDistinctCharsHasTheSameLengthOfStringToValidate(stringToValidate);
+    };
+  }
+
+  private boolean stringDistinctCharsHasTheSameLengthOfStringToValidate(String stringToValidate) {
+    return distinctChars(stringToValidate).length == stringToValidate.length();
+  }
+
+  private int[] distinctChars(String stringToValidate) {
+    return stringToValidate.chars()
+                           .distinct()
+                           .toArray();
   }
 }
